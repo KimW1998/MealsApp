0f2f12eed0b01fbbe6a2bde306c169b273e62d8c
Kim Wauben <72084242+KimW1998@users.noreply.github.com>
2022-07-29 11:52:33 +0200

    created a route param for navigating to the right overviewscreen

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -5,7 +5,9 @@ import CategoryGridTile from "../components/CategoryGridTile";
 function CategoriesScreen({ navigation }) {
   function renderCategroryItem(itemData) {
     function pressHandler() {
-      navigation.navigate("MealsOverview");
+      navigation.navigate("MealsOverview", {
+        categoryId: itemData.item.id,
+      }); 
     }
 
     return (
