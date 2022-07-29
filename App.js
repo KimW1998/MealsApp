7b240277f6dc9e46e29a69581eb49d003f2a2e98
Kim Wauben <72084242+KimW1998@users.noreply.github.com>
2022-07-29 11:14:48 +0200

    installed react navigation and imported it in the app.js

--- App.js
+++ App.js
@@ -1,16 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import CategoriesScreen from './screens/CategoriesScreen';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import CategoriesScreen from "./screens/CategoriesScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return ( <>
-    <StatusBar style='light'/>
-    <CategoriesScreen />
+  return (
+    <>
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component = {CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-  },
+  container: {},
 });
