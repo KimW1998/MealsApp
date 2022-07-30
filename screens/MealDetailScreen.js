4d45d58ec7ea312df6476e91c3aacffc191691c5
Kim Wauben <72084242+KimW1998@users.noreply.github.com>
2022-07-30 14:18:51 +0200

    used the context api for creating a favorites list

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,26 +1,50 @@
-import { Text, View, Image, StyleSheet, ScrollView, Button } from "react-native";
-import { useLayoutEffect } from "react";
+import {
+  Text,
+  View,
+  Image,
+  StyleSheet,
+  ScrollView,
+  Button,
+} from "react-native";
+import { useContext, useLayoutEffect } from "react";
 import List from "../components/MealDetail/List";
 import Subtitle from "../components/MealDetail/Subtitle";
 import MealDetails from "../components/MealDetails";
 
 import { MEALS } from "../data/dummy-data";
 import IconButton from "../components/IconButton";
+import { FavoritesContext } from "../store/context/favorites-context";
 
 function MealDetailScreen({ route, navigation }) {
+  const favoriteMealsCtx = useContext(FavoritesContext);
+
   const mealId = route.params.mealId;
 
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
-function headerButtonPressHandler() {}
+  const mealIsFavorite = favoriteMealsCtx.ids.includes(mealId);
+
+  function changeFavoriteStatusHandler() {
+    if (mealIsFavorite) {
+      favoriteMealsCtx.removeFavorite(mealId);
+    } else {
+      favoriteMealsCtx.addFavorite(mealId);
+    }
+  }
 
   useLayoutEffect(() => {
     navigation.setOptions({
       headerRight: () => {
-        return <IconButton icon="star" color="white" onPress={headerButtonPressHandler} />;
-      }
-    })
-  }, [navigation, headerButtonPressHandler]);
+        return (
+          <IconButton
+            icon={mealIsFavorite ? "star" : "star-outline"}
+            color="white"
+            onPress={changeFavoriteStatusHandler}
+          />
+        );
+      },
+    });
+  }, [navigation, changeFavoriteStatusHandler]);
 
   return (
     <ScrollView style={styles.rootContainer}>
@@ -34,11 +58,11 @@ function headerButtonPressHandler() {}
       />
       <View style={styles.listOuterContainer}>
         <View style={styles.listContainer}>
-        <Subtitle>ingredients</Subtitle>
-        <List data={selectedMeal.ingredients} />
-        <Subtitle>Steps</Subtitle>
-        <List data={selectedMeal.steps} />
-      </View>
+          <Subtitle>ingredients</Subtitle>
+          <List data={selectedMeal.ingredients} />
+          <Subtitle>Steps</Subtitle>
+          <List data={selectedMeal.steps} />
+        </View>
       </View>
     </ScrollView>
   );
@@ -47,9 +71,9 @@ function headerButtonPressHandler() {}
 export default MealDetailScreen;
 
 const styles = StyleSheet.create({
-    rootContainer: {
-        marginBottom: 32,
-    },
+  rootContainer: {
+    marginBottom: 32,
+  },
   image: {
     width: "100%",
     height: 350,
@@ -65,9 +89,9 @@ const styles = StyleSheet.create({
     color: "white",
   },
   listContainer: {
-    width: '80%',
+    width: "80%",
   },
   listOuterContainer: {
-    alignItems: 'center',
+    alignItems: "center",
   },
 });
